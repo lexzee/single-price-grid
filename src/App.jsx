a8c126f9ff9007b0819364ed011e4e84173a03b3
Abdulquddus Abdulrahman (Lexzee) <developerlexzee@gmail.com>
2023-06-26 00:15:38 +0100

    basic html skeleton

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,6 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
@@ -35,6 +31,12 @@ function App() {
           </p>
         </div>
       </div>
+      <footer>
+    <p class="attribution">
+      Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>.
+      Coded by <a href="#">Your Name Here</a>.
+    </p>
+  </footer>
     </>
   )
 }
