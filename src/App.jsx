fecd9af846eab9a1e84b3da76e1cdfc7362f54cb
Abdulquddus Abdulrahman (Lexzee) <developerlexzee@gmail.com>
2023-06-26 00:12:35 +0100

    basic html skeleton

--- src/App.jsx
+++ src/App.jsx
@@ -8,26 +8,33 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="App">
+        <div className="section1">
+          <h1 className="header">Join our community</h1>
+          <p className="caption">30-day, hassle-free money back guarantee</p>
+          <p className="description">Gain access to our full library of tutorials along with expert code reviews. Perfect for any developers who are serious about honing their skills.</p>
+        </div>
+        <div className="section2">
+          <h1 className="header">Monthly Subscription</h1>
+          <p className="price">$29 <span className="duration">per month</span></p>
+          <p className="description">Full access for less than $1 a day</p>
+          <button className="signup">Sign Up</button>
+        </div>
+        <div className="section3">
+          <h1 className="header">
+            Why us
+          </h1>
+          <p className="description">
+            Tutorials by industry experts <br />
+            Peer & expert code review <br />
+            Coding exercises <br />
+            Access to our GitHub repos <br />
+            Community forum <br />
+            Flashcard decks <br />
+            New videos every week
+          </p>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
