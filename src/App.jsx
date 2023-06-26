989b331d7f010e0efb040da70b686a7324a5aac8
Abdulquddus Abdulrahman (Lexzee) <developerlexzee@gmail.com>
2023-06-26 18:27:30 +0100

    desktop styling done

--- src/App.jsx
+++ src/App.jsx
@@ -34,7 +34,7 @@ function App() {
       <footer>
     <p className="attribution">
       Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>.
-      Coded by <a href="#">Your Name Here</a>.
+      Coded by <a href="https://github.com/lexzee">Lexzee</a>.
     </p>
   </footer>
     </>
